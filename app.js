8261780e2ce4fee16bb0ba64f1542157b5fd1219
Esteban Mejia <estebanmejia277@gmail.com>
2019-05-19 17:37:48 -0500

    Saved data in local storage

--- app.js
+++ app.js
@@ -1,25 +1,51 @@
 const app = new Vue({
     el: '#app',
-    data:{
-        title: 'Todo application',
+    data: {
+        title: 'Please enter a task',
         tasks: [],
-        newTask : '',
+        newTask: '',
     },
-    methods:{
-        addTask(){
-            this.tasks.push({
-                name: this.newTask,
-                status: false
-            });
-            this.newTask = "";
+    methods: {
+        addTask() {
+            if (this.newTask !== '') {
+                document.getElementById('warning').innerHTML = "";
+                this.tasks.push({
+                    name: this.newTask,
+                    status: false
+                });
+                this.saveInLocalStorage('tasks-local', this.tasks);
+                this.newTask = "";
+            }else{
+                document.getElementById('warning').innerHTML = `
+                <div class="alert alert-warning">
+                    Please enter a <strong>task</strong>
+                </div>
+                `
+            }
+
         },
-        taskDone(index){
+        taskDone(index) {
             this.tasks[index].status = true;
+            this.saveInLocalStorage('tasks-local', this.tasks);
         },
-        deleteTask(index){
+        deleteTask(index) {
             this.tasks.splice(index, 1);
+            this.saveInLocalStorage('tasks-local', this.tasks);
+        },
+        editTask(index) {
+
+        },
+        saveInLocalStorage(storageName, varToSave) {
+            localStorage.setItem(storageName, JSON.stringify(varToSave));
         }
-
+    },
+    created() {
+        let db = JSON.parse(localStorage.getItem('tasks-local'));
+        console.log(db);
+        if (db === null) {
+            this.tasks = []
+        } else {
+            this.tasks = db;
+        }
     }
-
 });
